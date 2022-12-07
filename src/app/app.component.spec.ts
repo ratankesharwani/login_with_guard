6e1bd08d2a1de52d692d0aedb28f51db0b448a66
ratan <ratan.kesharwani@rationalfx.com>
2022-12-07 18:16:31 +0530

    ok

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'MyProject10'`, () => {
+  it(`should have as title 'project5'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('MyProject10');
+    expect(app.title).toEqual('project5');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('MyProject10 app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('project5 app is running!');
   });
 });
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,37 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { EmployeeComponent } from './employee/employee.component';
+import { EmplistingComponent } from './emplisting/emplisting.component';
+import { EmpaddComponent } from './empadd/empadd.component';
+import {HttpClientModule, HTTP_INTERCEPTORS} from '@angular/common/http'
+import {FormsModule, ReactiveFormsModule} from '@angular/forms'
+import { TokenInterceptorService } from './service/token-interceptor.service';
+import {EmployeeService} from './service/employee.service';
+import { UserComponent } from './user/user.component';
+import { AboutComponent } from './about/about.component'
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    LoginComponent,
+    EmployeeComponent,
+    EmplistingComponent,
+    EmpaddComponent,
+    UserComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,ReactiveFormsModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [EmployeeService,{provide:HTTP_INTERCEPTORS,useClass:TokenInterceptorService,multi:true}],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
